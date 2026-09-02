b2dbc4edd973e057d0ec2e8d4f2e7cd470a1d65c
anara-ai <prajoth@anara.ai>
2026-09-01 22:33:18 -0400

    Redesign: player pages, search, new Brief tab, team colours, tier names

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var C="draft-v1";
+var C="draft-v2";
 self.addEventListener("install",function(e){
   e.waitUntil(caches.open(C).then(function(c){return c.addAll(["./","./index.html","./data.js","./manifest.json"]);}).then(function(){return self.skipWaiting();}));
 });
